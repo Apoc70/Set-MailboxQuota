bc60361d4b812c599be8d1de899c76d04e0764de
Thomas Stensitzki <thomas@mcsmemail.de>
2015-09-18 10:52:29 +0200

    Refactored to functions

--- Set-MailboxQuota.ps1
+++ Set-MailboxQuota.ps1
@@ -1,13 +1,13 @@
 <#
     .SYNOPSIS
-    Set default mailbox quotas per Exchange Server 2013 mailbox
+    Set default mailbox and mailbox database quotas per Exchange Server 2013 mailbox or mailbox database
    
    	Thomas Stensitzki
 	
 	THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
 	RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 	
-	Version 1.0, 2015-056-03
+	Version 1.0, 2015-09-18
 
     Ideas, comments and suggestions to support@granikos.eu 
 	
@@ -23,6 +23,8 @@
     Revision History 
     -------------------------------------------------------------------------------- 
     1.0     Initial release 
+    1.1     Archive support added
+    1.2     Refactored to functions
 	
 	.PARAMETER MailboxMaxSize  
     Maximum mailbox size (aka Prohibit Send and Receive) in GB
@@ -62,7 +64,9 @@ Param(
     [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="AllDB")]
         [switch]$AllDatabases,
     [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="UserMB")]
-        [string]$EmailAddress
+        [string]$EmailAddress,
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="UserMB")]
+        [switch]$Archive
 )
 
 function Request-Choice {
@@ -75,11 +79,90 @@ function Request-Choice {
     return $choiceReturn   
 }
 
+function SetMailboxquota {
+    param([string]$EmailAddress)
+    try {
+
+        Write-Verbose "Fetching user mailbox $($EmailAddress)"
+
+        $usermailbox = Get-Mailbox $EmailAddress
+        Write-Host
+
+        # Configure user mailbox settings
+            Write-Host "Current quotas of User : $($usermailbox.DisplayName)"
+
+            if($usermailbox.UseDatabaseQuotaDefaults) {
+                Write-Host "User mailbox is set to use DATABASE DEFAULTS!"
+            }
+            else {
+                Write-Host "User mailbox is set to use mailbox QUOTA OVERRIDES!"
+                
+                if($usermailbox.ProhibitSendReceiveQuota -ne "unlimited") {
+                    Write-Host "ProhibitSendReceive [MB]: $($usermailbox.ProhibitSendReceiveQuota.Value.ToMB())"
+                    Write-Host "Prohibit Send Size  [MB]: $($usermailbox.ProhibitSendQuota.Value.ToMB())"
+                    Write-Host "Issue Warning Size  [MB]: $($usermailbox.IssueWarningQuota.Value.ToMB())"
+                }
+                else {
+                    Write-Host "User mailbox is set to unlimited quota"
+                }
+            }
+
+            if((Request-Choice -Caption "Do you want to change the quotas of mailbox $($usermailbox.DisplayName)?") -eq 0) {
+                # Set user mailbox quotas
+                Write-Host "Setting selected quotas for mailbox $($usermailbox.DisplayName)"
+
+                $logger.Write("Setting mailbox $($EmailAddress) to ProhibitSendReceive $($ProhibitSendReceiveSize)MB, Prohibit Send Size $($ProhibitSendSize)MB, Issue Warning Size $($IssueWarningSize)MB")
+
+                Get-Mailbox $EmailAddress | Set-Mailbox -IssueWarningQuota "$($IssueWarningSize)MB" -ProhibitSendQuota "$($ProhibitSendSize)MB" -ProhibitSendReceiveQuota "$($ProhibitSendReceiveSize)MB" -UseDatabaseQuotaDefaults:$false
+            }
+        }
+    catch {
+        Write-Error "Error fetching mailbox data. Please verify email address!"
+    }
+}
+
+function SetArchivequota {
+    param([string]$EmailAddress)
+    try {
+
+        Write-Verbose "Fetching user mailbox $($EmailAddress)"
+
+        $usermailbox = Get-Mailbox $EmailAddress
+        Write-Host
+    
+         # Configure user mailbox archive settings
+            Write-Host "Current archive quotas of User : $($usermailbox.DisplayName)"
+            if($usermailbox.ArchiveQuota.IsUnlimited -ne $true) {
+                Write-Host "Archive Quota Size [MB]        : $($usermailbox.ArchiveQuota.Value.ToMB())"
+            }
+            else {
+                Write-Host "Archive Quota Size [MB]        : Unlimited"
+            }
+            if($usermailbox.ArchiveWarningQuota.IsUnlimited -ne $true) {
+                Write-Host "Archive Warning Quota Size [MB]: $($usermailbox.ArchiveWarningQuota.Value.ToMB())"
+            }
+            else {
+                Write-Host "Archive Warning Quota Size [MB]: Unlimited"
+            }
+            if((Request-Choice -Caption "Do you want to change the archive quotas of mailbox $($usermailbox.DisplayName)?") -eq 0) {
+                # Set user mailbox archive quotas
+                Write-Host "Setting selected archive quotas for mailbox $($usermailbox.DisplayName)"
+                
+                $logger.Write("Setting archive $($EmailAddress) to ArchiveQuota $($ProhibitSendReceiveSize)MB, ArchiveWarningQuota $($IssueWarningSize)MB")
+
+                Get-Mailbox $EmailAddress | Set-Mailbox -ArchiveWarningQuota "$($IssueWarningSize)MB" -ArchiveQuota "$($ProhibitSendReceiveSize)MB"
+            }
+    }
+    catch {
+        Write-Error "Error fetching mailbox data. Please verify email address!"
+    }
+}
+
+
 Set-StrictMode -Version Latest
 
-# IMPORT GLOBAL MODULE
+# IMPORT GLOBAL MODULE AND SET INITIAL VARIABLES
 Import-Module GlobalFunctions
-
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $ScriptName = $MyInvocation.MyCommand.Name
 $logger = New-Logger -ScriptRoot $ScriptDir -ScriptName $ScriptName -LogFileRetention 14
@@ -90,11 +173,20 @@ $ProhibitSendSize = [System.ValueType]([math]::Round((($MaxMailboxSize * ($Prohi
 $IssueWarningSize = [System.ValueType]([math]::Round((($MaxMailboxSize * ($IssueWarningPercent / 100)) / 1MB)))
 
 Write-Host "New Quotas:"
-Write-Host "ProhibitSendReceive[MB]: $($ProhibitSendReceiveSize)"
-Write-Host "Prohibit Send [%]: $($ProhibitSendPercent)"
-Write-Host "Prohibit Send Size [MB]: $($ProhibitSendSize)"
-Write-Host "Issue Warning [%]: $($IssueWarningPercent)"
-Write-Host "Issue Warning Size [MB]: $($IssueWarningSize)"
+if(!($Archive)) {
+    # Display new quota settings for user mailbox
+    Write-Host "ProhibitSendReceive[MB]: $($ProhibitSendReceiveSize)"
+    Write-Host "Prohibit Send [%]: $($ProhibitSendPercent)"
+    Write-Host "Prohibit Send Size [MB]: $($ProhibitSendSize)"
+    Write-Host "Issue Warning [%]: $($IssueWarningPercent)"
+    Write-Host "Issue Warning Size [MB]: $($IssueWarningSize)"
+}
+else {
+    # Display new quota settings for user mailbox archive
+    Write-Host "Archive Quota Size   [MB]: $($ProhibitSendReceiveSize)"
+    Write-Host "Archive Warning [%]: $($IssueWarningPercent)"
+    Write-Host "Archive Warning Size [MB]: $($IssueWarningSize)"
+}
 
 if($AllDatabases) {
 
@@ -109,32 +201,15 @@ if($AllDatabases) {
 
 if(![string]::IsNullOrWhiteSpace($EmailAddress)) {
     try {
-
-        Write-Verbose "Fetching user mailbox $($EmailAddress)"
-
-        $usermailbox = Get-Mailbox $EmailAddress
-
-        Write-Host
-        Write-Host "Current quotas of User : $($usermailbox.DisplayName)"
-
-        if($usermailbox.UseDatabaseQuotaDefaults) {
-            Write-Host "User mailbox is set to use DATABASE DEFAULTS!"
+        if(!($Archive)) {
+            # Configure user mailbox settings
+            SetMailboxquota -EmailAddress $EmailAddress
+             }
+        
+        else { 
+            # Configure user mailbox archive settings
+            SetArchivequota -EmailAddress $EmailAddress 
         }
-        else {
-            Write-Host "User mailbox is set to use mailbox QUOTA OVERRIDES!"
-            Write-Host "ProhibitSendReceive[MB]: $($usermailbox.ProhibitSendReceiveQuota.Value.ToMB())"
-            Write-Host "Prohibit Send Size [MB]: $($usermailbox.ProhibitSendQuota.Value.ToMB())"
-            Write-Host "Issue Warning Size [MB]: $($usermailbox.IssueWarningQuota.Value.ToMB())"
-        }
-
-        if((Request-Choice -Caption "Do you want to change the quotas of mailbox $($usermailbox.DisplayName)?") -eq 0) {
-            Write-Host "Setting selected quotas for mailbox $($usermailbox.DisplayName)"
-
-            $logger.Write("Setting mailbox $($EmailAddress) to ProhibitSendReceive $($ProhibitSendReceiveSize)MB, Prohibit Send Size $($ProhibitSendSize)MB, Issue Warning Size $($IssueWarningSize)MB")
-
-            Get-Mailbox $EmailAddress | Set-Mailbox -IssueWarningQuota "$($IssueWarningSize)MB" -ProhibitSendQuota "$($ProhibitSendSize)MB" -ProhibitSendReceiveQuota "$($ProhibitSendReceiveSize)MB" -UseDatabaseQuotaDefaults:$false
-        }
-
     }
     catch {
         Write-Error "Error fetching mailbox data. Please verify email address!"
