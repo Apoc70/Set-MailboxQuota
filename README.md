b5e45d66a20fee4750b2309b98cfdaba0e1b706e
Thomas Stensitzki <thomas@mcsmemail.de>
2016-09-06 11:22:56 +0200

    PowerShell hygiene README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# Set-MailboxQuota
-Script to set mailbox quotas at database level or for a single mailbox.
+# Set-MailboxQuota.ps1
+Set default mailbox and mailbox database quotas per Exchange Server 2013/2016 mailbox or mailbox database
+
+##Description
+This script configures the IssueWarning, ProhibitSend and ProhibitSendAndReceive attributes for Exchange Server 2013/2016 databases
+
+##Inputs
+MailboxMaxSize  
+Maximum mailbox size (aka Prohibit Send and Receive) in GB
+
+ProhibitSendPercent
+Prohibit send % of maximum mailbox size (default = 90)
+
+IssueWarningPercent
+Issue warning % of maximum mailbox size (default = 80)
+
+AllDatabases
+Apply quotas to all Exchange Server 2013 mailbox databases
+
+EmailAddress
+Email address of user mailbox to apply quotas to
+
+##Outputs
+None
+
+##Examples
+```
+.\Set-MailboxQuota.ps1 -MaxMailboxSize 1GB -AllDatabases
+```
+et all Exchange 2013 databases to 1GB max mailbox size and use default percentage (90%/80%) for prohibit send and issue warning
+
+```
+.\Set-MailboxQuota.ps1 -EmailAddress usera@mcsmemail.de -MaxMailboxSize 10GB
+```
+Set max mailbox size to 10Gb for a dedicated user and use default percentage (90%/80%) for prohibit send and issue warning
+
+##TechNet Gallery
+Find the script at TechNet Gallery
+* https://gallery.technet.microsoft.com/Set-mailbox-quotas-at-c972c3f3
+
+##Blog Post
+Corresponding blog post
+* https://www.granikos.eu/en/justcantgetenough/PostId/214/set-mailbox-quotas-at-database-or-mailbox-level-the-simple-way
+
+
+##Credits
+Written by: Thomas Stensitzki
+
+Stay connected:
+
+* My Blog: http://justcantgetenough.granikos.eu
+* Twitter:	https://twitter.com/stensitzki
+* LinkedIn:	http://de.linkedin.com/in/thomasstensitzki
+* Github:	https://github.com/Apoc70
+
+For more Office 365, Cloud Security and Exchange Server stuff checkout services provided by Granikos
+
+* Blog:     http://blog.granikos.eu/
+* Website:	https://www.granikos.eu/en/
+* Twitter:	https://twitter.com/granikos_de
--- Set-MailboxQuota.ps1
+++ Set-MailboxQuota.ps1
@@ -1,80 +1,81 @@
 <#
-    .SYNOPSIS
-    Set default mailbox and mailbox database quotas per Exchange Server 2013 mailbox or mailbox database
+  .SYNOPSIS
+  Set default mailbox and mailbox database quotas per Exchange Server 2013 mailbox or mailbox database
    
-   	Thomas Stensitzki
+  Thomas Stensitzki
 	
 	THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
 	RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 	
-	Version 1.0, 2015-09-18
+	Version 1.3, 2016-09-06
 
-    Ideas, comments and suggestions to support@granikos.eu 
+  Ideas, comments and suggestions to support@granikos.eu 
 	
-    .DESCRIPTION
+  .DESCRIPTION
 	
-    This script configures the IssueWarning, ProhibitSend and ProhibitSendAndReceive attributes for Exchange Server 2013 databases
+  This script configures the IssueWarning, ProhibitSend and ProhibitSendAndReceive attributes for Exchange Server 2013 databases
 
-    .NOTES 
-    Requirements 
-    - Windows Server 2012 R2  
-    - Exchange Server 2013
+  .NOTES 
+  Requirements 
+  - Windows Server 2012 R2  
+  - Exchange Server 2013/2016 Management Shell
 
-    Revision History 
-    -------------------------------------------------------------------------------- 
-    1.0     Initial release 
-    1.1     Archive support added
-    1.2     Refactored to functions
+  Revision History 
+  -------------------------------------------------------------------------------- 
+  1.0     Initial release 
+  1.1     Archive support added
+  1.2     Refactored to functions
+  1.3     PowerShell hygiene
 	
-	.PARAMETER MailboxMaxSize  
-    Maximum mailbox size (aka Prohibit Send and Receive) in GB
+  .PARAMETER MailboxMaxSize  
+  Maximum mailbox size (aka Prohibit Send and Receive) in GB
 
-    .PARAMETER ProhibitSendPercent
-    Prohibit send % of maximum mailbox size (default = 90)
+  .PARAMETER ProhibitSendPercent
+  Prohibit send % of maximum mailbox size (default = 90)
 
-    .PARAMETER IssueWarningPercent
-    Issue warning % of maximum mailbox size (default = 80)
+  .PARAMETER IssueWarningPercent
+  Issue warning % of maximum mailbox size (default = 80)
 
-    .PARAMETER AllDatabases
-    Apply quotas to all Exchange Server 2013 mailbox databases
+  .PARAMETER AllDatabases
+  Apply quotas to all Exchange Server 2013 mailbox databases
 
-    .PARAMETER EmailAddress
-    Email address of user mailbox to apply quotas to
+  .PARAMETER EmailAddress
+  Email address of user mailbox to apply quotas to
 
-    .EXAMPLE
-    Set all Exchange 2013 databases to 1GB max mailbox size and use default percentage (90%/80%) for prohibit send and issue warning
-    .\Set-MailboxQuota.ps1 -MaxMailboxSize 1GB -AllDatabases
+  .EXAMPLE
+  Set all Exchange 2013 databases to 1GB max mailbox size and use default percentage (90%/80%) for prohibit send and issue warning
+  .\Set-MailboxQuota.ps1 -MaxMailboxSize 1GB -AllDatabases
 
-    .EXAMPLE
-    Set max mailbox size to 10Gb for a dedicated user and use default percentage (90%/80%) for prohibit send and issue warning
-    .\Set-MailboxQuota.ps1 -EmailAddress usera@mcsmemail.de -MaxMailboxSize 10GB
+  .EXAMPLE
+  Set max mailbox size to 10Gb for a dedicated user and use default percentage (90%/80%) for prohibit send and issue warning
+  .\Set-MailboxQuota.ps1 -EmailAddress usera@mcsmemail.de -MaxMailboxSize 10GB
 
-    #>
+#>
 
 Param(
-    [parameter(Mandatory=$true,ValueFromPipeline=$false,ParameterSetName="AllDB")]
-    [parameter(Mandatory=$true,ValueFromPipeline=$false,ParameterSetName="UserMB")]
+    [parameter(Mandatory=$true,ValueFromPipeline=$false,ParameterSetName='AllDB')]
+    [parameter(Mandatory=$true,ValueFromPipeline=$false,ParameterSetName='UserMB')]
         [int64]$MaxMailboxSize,
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="AllDB")]
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="UserMB")]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='AllDB')]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='UserMB')]
         [int]$ProhibitSendPercent=90,
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="AllDB")]
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="UserMB")]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='AllDB')]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='UserMB')]
         [int]$IssueWarningPercent=80,
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="AllDB")]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='AllDB')]
         [switch]$AllDatabases,
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="UserMB")]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='UserMB')]
         [string]$EmailAddress,
-    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName="UserMB")]
+    [parameter(Mandatory=$false,ValueFromPipeline=$false,ParameterSetName='UserMB')]
         [switch]$Archive
 )
 
 function Request-Choice {
     param([string]$Caption)
-    $choices =  [System.Management.Automation.Host.ChoiceDescription[]]@("&Yes","&No")
+    $choices =  [System.Management.Automation.Host.ChoiceDescription[]]@('&Yes','&No')
     [int]$defaultChoice = 1
 
-    $choiceReturn = $Host.UI.PromptForChoice($Caption, "", $choices, $defaultChoice)
+    $choiceReturn = $Host.UI.PromptForChoice($Caption, '', $choices, $defaultChoice)
 
     return $choiceReturn   
 }
@@ -92,18 +93,18 @@ function SetMailboxquota {
             Write-Host "Current quotas of User : $($usermailbox.DisplayName)"
 
             if($usermailbox.UseDatabaseQuotaDefaults) {
-                Write-Host "User mailbox is set to use DATABASE DEFAULTS!"
+                Write-Host 'User mailbox is set to use DATABASE DEFAULTS!'
             }
             else {
-                Write-Host "User mailbox is set to use mailbox QUOTA OVERRIDES!"
+                Write-Host 'User mailbox is set to use mailbox QUOTA OVERRIDES!'
                 
-                if($usermailbox.ProhibitSendReceiveQuota -ne "unlimited") {
+                if($usermailbox.ProhibitSendReceiveQuota -ne 'unlimited') {
                     Write-Host "ProhibitSendReceive [MB]: $($usermailbox.ProhibitSendReceiveQuota.Value.ToMB())"
                     Write-Host "Prohibit Send Size  [MB]: $($usermailbox.ProhibitSendQuota.Value.ToMB())"
                     Write-Host "Issue Warning Size  [MB]: $($usermailbox.IssueWarningQuota.Value.ToMB())"
                 }
                 else {
-                    Write-Host "User mailbox is set to unlimited quota"
+                    Write-Host 'User mailbox is set to unlimited quota'
                 }
             }
 
@@ -117,7 +118,7 @@ function SetMailboxquota {
             }
         }
     catch {
-        Write-Error "Error fetching mailbox data. Please verify email address!"
+        Write-Error 'Error fetching mailbox data. Please verify email address!'
     }
 }
 
@@ -136,13 +137,13 @@ function SetArchivequota {
                 Write-Host "Archive Quota Size [MB]        : $($usermailbox.ArchiveQuota.Value.ToMB())"
             }
             else {
-                Write-Host "Archive Quota Size [MB]        : Unlimited"
+                Write-Host 'Archive Quota Size [MB]        : Unlimited'
             }
             if($usermailbox.ArchiveWarningQuota.IsUnlimited -ne $true) {
                 Write-Host "Archive Warning Quota Size [MB]: $($usermailbox.ArchiveWarningQuota.Value.ToMB())"
             }
             else {
-                Write-Host "Archive Warning Quota Size [MB]: Unlimited"
+                Write-Host 'Archive Warning Quota Size [MB]: Unlimited'
             }
             if((Request-Choice -Caption "Do you want to change the archive quotas of mailbox $($usermailbox.DisplayName)?") -eq 0) {
                 # Set user mailbox archive quotas
@@ -154,7 +155,7 @@ function SetArchivequota {
             }
     }
     catch {
-        Write-Error "Error fetching mailbox data. Please verify email address!"
+        Write-Error 'Error fetching mailbox data. Please verify email address!'
     }
 }
 
@@ -166,13 +167,13 @@ Import-Module GlobalFunctions
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $ScriptName = $MyInvocation.MyCommand.Name
 $logger = New-Logger -ScriptRoot $ScriptDir -ScriptName $ScriptName -LogFileRetention 14
-$logger.Write("Script started")
+$logger.Write('Script started')
 
 $ProhibitSendReceiveSize = [System.ValueType]([math]::Round(($MaxMailboxSize / 1MB)))
 $ProhibitSendSize = [System.ValueType]([math]::Round((($MaxMailboxSize * ($ProhibitSendPercent / 100)) / 1MB)))
 $IssueWarningSize = [System.ValueType]([math]::Round((($MaxMailboxSize * ($IssueWarningPercent / 100)) / 1MB)))
 
-Write-Host "New Quotas:"
+Write-Host 'New Quotas:'
 if(!($Archive)) {
     # Display new quota settings for user mailbox
     Write-Host "ProhibitSendReceive[MB]: $($ProhibitSendReceiveSize)"
@@ -190,8 +191,8 @@ else {
 
 if($AllDatabases) {
 
-    if((Request-Choice -Caption "Do you want to change the quotas of all mailbox databases?") -eq 0) {
-        Write-Host "Setting selected quotas for all mailbox databases!" 
+    if((Request-Choice -Caption 'Do you want to change the quotas of all mailbox databases?') -eq 0) {
+        Write-Host 'Setting selected quotas for all mailbox databases!' 
         $logger.Write("Setting all Exchange 2013 mailbox databases to ProhibitSendReceive $($ProhibitSendReceiveSize)MB, Prohibit Send Size $($ProhibitSendSize)MB, Issue Warning Size $($IssueWarningSize)MB")
         # fetch only NON recovery databases
         Get-MailboxDatabase | ?{$_.Recovery -eq $false} | Set-MailboxDatabase -IssueWarningQuota "$($IssueWarningSize)MB" -ProhibitSendQuota "$($ProhibitSendSize)MB" -ProhibitSendReceiveQuota "$($ProhibitSendReceiveSize)MB"
@@ -212,9 +213,9 @@ if(![string]::IsNullOrWhiteSpace($EmailAddress)) {
         }
     }
     catch {
-        Write-Error "Error fetching mailbox data. Please verify email address!"
+        Write-Error 'Error fetching mailbox data. Please verify email address!'
     }
 }
 
-Write-Host "Script finished!"
-$logger.Write("Script finished")
+Write-Host 'Script finished!'
+$logger.Write('Script finished')
